961d2292544fd96c92702c37f28c1bda70c01701
AgusAchille <agustin.a.fr@gmail.com>
2021-01-24 12:05:40 -0300

    Added current time to server log when init

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+var now = new Date();
+console.log(now.toLocaleTimeString());
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
