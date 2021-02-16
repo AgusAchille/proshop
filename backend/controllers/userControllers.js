b086712d6adb8b64cbf9237f06d2e801b47de113
AgusAchille <agustin.a.fr@gmail.com>
2021-02-16 01:55:39 -0300

    70. Get User By ID & Update User Endpoints

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -140,3 +140,44 @@ export const deleteUser = asyncHandler(async (req, res) => {
         throw new Error('user not found');
     }
 })
+
+// @desc    Get user by id
+// @route   GET /api/users/:id
+// @access  Private/Admin
+export const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password');
+
+    if(user){
+        res.json(user);
+    }
+    else {
+        res.status(404);
+        throw new Error('user not found'); 
+    }
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+export const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    
+    if(user) {
+        if(req.body.name && req.body.name.trim()) user.name = req.body.name.trim();
+        if(req.body.email && req.body.email.trim()) user.email = req.body.email.trim();
+        if(req.body.isAdmin !== undefined) user.isAdmin = req.body.isAdmin;
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    }
+    else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+})
